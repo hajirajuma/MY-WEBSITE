622be6053fdd4fb66ddd070f8be740d46b751447
hajirajuma <hajiraj475@gmail.com>
2025-06-17 15:49:45 -0700

    styling

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const check = '../assets/check.svg';
+const star = '../assets/star.svg';
+const sushi12 = '../assets/sushi-12.png';
+const sushi11 =  '../assets/sushi-11.png';
+const sushi10 = '../assets/sushi-10.png';
+
+//init AOS ANIMATION
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+const trendingSushis = [
+    'Make Sushi',
+    'Nigiri Sushi',
+    'Oshizushi',
+    'Temaki Sushi',
+    'Uramaki Sushi',
+    'Inari sushi'
+];
+
